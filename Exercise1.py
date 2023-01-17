5b16da38230bd39c440fcc70a8c2c12cabfd417d
Nikhil Jha <jhanikhil19@gmail.com>
2023-01-17 19:58:33 +0530

    DAY2

---
+++ Exercise1.py
@@ -0,0 +1,6 @@
+#Apni dictionary
+
+d1={"Apple":"Apple is a fruit","Ball":"Ball is a spherical object used for playing","cat":"Cat is an animal"}
+print("Enter a word to know the meaning")
+word= input()
+print("Meaning of the",word,"is",d1[word])
